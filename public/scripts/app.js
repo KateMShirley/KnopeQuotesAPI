bcfcca2658e990ff6340b1c7231a79801c7edb00
KateMShirley <kate.m.shirley@gmail.com>
2015-09-01 11:40:40 -0500

    REVERTED FROM 8.31

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -120,4 +120,7 @@ $(document).ready(function(event) {
     $('.men').on('click', function() {
       active.collectionView.renderRandoMen();
     })
+
+    $('.leslie').fadeIn("fast",function() {
+    });
 });
